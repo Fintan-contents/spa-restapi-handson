194a14abf51e26a85303954462137c65d45ea93a
sinho-lee <i.shinho.695@tis.co.jp>
2023-11-13 10:28:14 +0900

    【starter-kit】ReactDOMを使用するように修正

--- starter-kit/frontend/src/index.tsx
+++ starter-kit/frontend/src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render (
+const root = ReactDOM.createRoot(container!);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
